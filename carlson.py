3f177d53bf764305d7710dff42f3e853339d9dc8
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-07 09:25:53 -0500

    Update carlson.py

--- carlson.py
+++ carlson.py
@@ -1,3 +1,9 @@
+Shop = [ 
+{ "weapon" : "Iron Sword", "attack" : 35},
+{ "weapon" : "Ice Sword", "attack" : 50},
+{ "weapon" : "Fire Sword", "attack" : 60}
+]
+
 class player:
     def __init__(self, name, money, inventory):
         self.name = name
@@ -7,11 +13,14 @@ class player:
         self.money += value
     def show_balance(self):
         print(f"You have {self.money}")
+    def buy(self, item):
+        self.inventory.append(item)
 
 name = input("Name your hero")
-name = player(f"{name}", 100, ["wooden sword"])
-name.inventory.append("Ice Sword")
+name = player(f"{name}", 100, [{"weapon": "wooden sword", "attack" : 15}])
 for key, value in name.__dict__.items():
     print(f"{key}: {value}")
+
+
 
 
