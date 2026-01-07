4d7125e1e87332c51004af1f329e7a8f0e34ea6f
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-07 09:15:09 -0500

    Update carlson.py

--- carlson.py
+++ carlson.py
@@ -3,3 +3,15 @@ class player:
         self.name = name
         self.money = money
         self. inventory = inventory
+    def earn(self, value):
+        self.money += value
+    def show_balance(self):
+        print(f"You have {self.money}")
+
+name = input("Name your hero")
+name = player(f"{name}", 100, ["wooden sword"])
+name.inventory.append("Ice Sword")
+for key, value in name.__dict__.items():
+    print(f"{key}: {value}")
+
+
