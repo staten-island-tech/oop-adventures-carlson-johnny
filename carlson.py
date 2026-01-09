25b9ef60908f31a864d99644b2b40fcf0b4e9756
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-09 09:10:30 -0500

    Update carlson.py

--- carlson.py
+++ carlson.py
@@ -5,12 +5,13 @@ Shop = [
 ]
 
 class player:
-    def __init__(self, name, money, inventory):
+    def __init__(self, name, money, inventory, money_buff):
         self.name = name
         self.money = money
         self.inventory = inventory
+        self.money_buff = money_buff
     def earn(self, value):
-        self.money += value
+        self.money += value * self.money_buff
     def show_balance(self):
         print(f"You have {self.money}")
     def buy(self, item):
@@ -21,19 +22,24 @@ class player:
         
 
 name = input("Name your hero")
-name = player(f"{name}", 100, [{"weapon": "wooden sword", "attack" : 15}])
+name = player(f"{name}", 100, [{"weapon": "wooden sword", "attack" : 15}], 1.0)
+name.money_buff(1.2)
+name.earn(10)
 for key, value in name.__dict__.items():
     print(f"{key}: {value}")
 
+'''Do = input("Choose something to do. 1.Adventure.")
+if Do == "Adventure":'''
 
 
-shopping = input("buy something")
+'''shopping = input("buy something")
 for item in Shop:
     if shopping in item["weapon"]:
         name.buy(item)
         name.spend(item["price"])
         for key, value in name.__dict__.items():
-            print(f"{key}: {value}")
+            print(f"{key}: {value}")'''
+
 
 
 
