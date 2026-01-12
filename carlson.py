cd01dbedaef97320b23fe7c9d86706137788a160
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-12 09:26:16 -0500

    Update carlson.py

--- carlson.py
+++ carlson.py
@@ -18,27 +18,25 @@ class player:
         self.inventory.append(item)
     def spend(self, value):
         name.money -= value
-    '''def multipliers(self, value):'''
+    
         
 
 name = input("Name your hero")
 name = player(f"{name}", 100, [{"weapon": "wooden sword", "attack" : 15}], 1.0)
-name.money_buff = 1.2
-name.earn(10000)
-for key, value in name.__dict__.items():
-    print(f"{key}: {value}")
-
-'''Do = input("Choose something to do. 1.Adventure.")
-if Do == "Adventure":'''
 
 
-'''shopping = input("buy something")
-for item in Shop:
-    if shopping in item["weapon"]:
-        name.buy(item)
-        name.spend(item["price"])
-        for key, value in name.__dict__.items():
-            print(f"{key}: {value}")'''
+Do = input("Choose something to do. 1.Adventure.  2.Shop")
+if Do == "Adventure":
+    print("coming soon")
+
+if Do == "Shop":
+    shopping = input("buy something")
+    for item in Shop:
+        if shopping in item["weapon"]:
+            name.buy(item)
+            name.spend(item["price"])
+            for key, value in name.__dict__.items():
+                print(f"{key}: {value}")
 
 
 
