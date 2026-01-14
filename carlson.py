7533efa03369c539fdbbf7a875a7fddc4e68ed5c
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-14 09:26:14 -0500

    Update carlson.py

--- carlson.py
+++ carlson.py
@@ -27,16 +27,22 @@ name = player(f"{name}", 100, [{"weapon": "wooden sword", "attack" : 15}], 1.0)
 
 Do = input("Choose something to do. 1.Adventure.  2.Shop")
 if Do == "Adventure":
-    print("coming soon")
+    print("Starting Adventure")
+    
 
 if Do == "Shop":
     shopping = input("buy something")
+    found = False
     for item in Shop:
         if shopping in item["weapon"]:
             name.buy(item)
             name.spend(item["price"])
-            for key, value in name.__dict__.items():
-                print(f"{key}: {value}")
+            found = True
+            break
+        if shopping not in item["weapon"]:
+            print("item not found")
+        for key, value in name.__dict__.items():
+            print(f"{key}: {value}")
 
 
 
