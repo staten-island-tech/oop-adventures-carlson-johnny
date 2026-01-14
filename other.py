6c18df74cd68e227da4a58c40378c3b25f426ba0
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-14 09:12:02 -0500

    Update other.py

--- other.py
+++ other.py
@@ -3,6 +3,12 @@ class mobs:
         self.health = health
         self.attack = attack
         self.drops = drops
+        
 class Ogre(mobs):
-    def __init__(self, health, attack, drops):
-        super().__init__(health, attack, drops)
+    def __init__(self, name, health, attack, drops):
+        self.name = name
+        super().__init__(name, health, attack, drops)
+
+x = Ogre("Ogre", 100, 10, 10)
+for key, item in x.__dict__.items():
+    print(f"{key} : {item}")
